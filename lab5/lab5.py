df97ff4c49f83aff755d631b6d73a38d82fc59e3
Xar83 <40188397+Xar83@users.noreply.github.com>
2019-03-16 17:39:44 -0600

    drag working

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,5 +1,5 @@
 from vpython import *
-from math import sin, cos
+from math import sin, cos, radians
 import argparse
 
 
@@ -8,6 +8,7 @@ def set_scene(data):
     """
     Set Vpython Scene
     """
+    scene = canvas(backround=color.blue)
     scene.title = "Assignment 5: Projectile motion"
     scene.width = 800
     scene.heigth = 600
@@ -16,29 +17,57 @@ def set_scene(data):
     On a two-button mouse, middle is left + right.
     Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
     scene.forward = vector(0, -.3, -1)
+    scene.range = 50
     scene.x = -1
+    scene.background = color.blue
     # Set background: floor, table, etc
-    box(pos = vector(0,0,0), length=200, height=100, width=1)
+    box(pos = vector(0,0,0), length=2000, height=.1, width=1000, color=color.green)
 
 
 def motion_no_drag(data):
     """
     Create animation for projectile motion with no dragging force
     """
-    ball_nd = sphere(pos=vector(-25, data['init_height'], 0),
+    ball_nd = sphere(pos=vector(-80, data['init_height'], 0),
                         radius=1, color=color.cyan, make_trail=True)
     # Follow the movement of the ball
     scene.camera.follow(ball_nd)
-    # Set initial velocity & position
-    ball.velocity=vector(0,5,0)
+    # Set initial velocity & position angle requres radians 
+    ball_nd.velocity=rotate(vector(data['init_velocity'],0,0), angle=radians(data['theta']))
+    ball_nd.mass = data['ball_mass']
+    ball_nd.p=ball_nd.velocity*ball_nd.mass
+    grav=vector(0,data['gravity'],0)
+    netForce=grav*ball_nd.mass
+    
+    t = 0
     # Animate
-
+    while ball_nd._pos.y > ball_nd.radius:
+        rate(300)
+        ball_nd.pos=ball_nd.pos + (ball_nd.p/ball_nd.mass)*data['deltat']
+        ball_nd.p = ball_nd.p + netForce*data['deltat']
+        t = t + data['deltat']
 
 def motion_drag(data):
     """
-    Create animation for projectile motion with no dragging force
+    Create animation for projectile motion with dragging force
     """
-    pass
+    ball_nd = sphere(pos=vector(-80, data['init_height'], 0),
+                        radius=1, color=color.purple, make_trail=True)
+    # Set initial velocity & position
+    ball_nd.velocity=rotate(vector(data['init_velocity'],0,0), angle=radians(data['theta']))
+    ball_nd.mass = data['ball_mass']
+    ball_nd.p=ball_nd.velocity*ball_nd.mass
+    grav=vector(0,data['gravity'],0)
+    netForce=((grav*ball_nd.mass) - (data['alpha']*mag(ball_nd.p/ball_nd.mass)**2 * (ball_nd.p/mag(ball_nd.p)))) 
+
+    t = 0
+    # Animate
+    while ball_nd._pos.y > ball_nd.radius:
+        rate(300)
+        ball_nd.pos=ball_nd.pos + (ball_nd.p/ball_nd.mass)*data['deltat']
+        netForce = ((grav*ball_nd.mass) - (data['alpha']*mag(ball_nd.p/ball_nd.mass)**2 * (ball_nd.p/mag(ball_nd.p)))) 
+        ball_nd.p = ball_nd.p + netForce * data['deltat']
+        t = t + data['deltat']
 
 
 def main():
@@ -73,11 +102,11 @@ def main():
     data['alpha'] = data['rho'] * data['Cd'] * data['ball_area'] / 2.0
     data['beta'] = data['alpha'] / data['ball_mass']
     # Set Scene
-#    set_scene(data)
+    set_scene(data)
     # 2) No Drag Animation
-#    motion_no_drag(data)
+    motion_no_drag(data)
     # 3) Drag Animation
-#     motion_drag(data)
+    motion_drag(data)
     # 4) Plot Information: extra credit
 #     plot_data(data)
 
